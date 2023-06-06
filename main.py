3faf833471a3e100708ce9587e2cf885481d10b9
21-072 M. Mahdum Al-A'Lam <102146028+alammahdum8@users.noreply.github.com>
2023-06-06 12:28:46 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 '''
 Created on Feb 13, 2014
-
 @author: sushant
 '''
 from tkinter import *
 from sklearn.cluster import DBSCAN
 from dbscanner import DBScanner
-import numpy as np
+import numpy as np
 import re, csv, sys
-
 
 CONFIG = 'config'
 DATA = 'data/abc.csv'
@@ -19,42 +17,35 @@ def get_data(config):
         csv_reader = csv.reader(file_obj)
         for id_, row in enumerate(csv_reader):
             if len(row) < config['dim']:
-                print ("ERROR: The data you have provided has fewer \
-                    dimensions than expected (dim = %d < %d)"
-                    % (config['dim'], len(row)))
+                print("ERROR: The data you have provided has fewer dimensions than expected (dim = %d < %d)" % (config['dim'], len(row)))
                 sys.exit()
             else:
-                point = {'id':id_, 'value': []}
+                point = {'id': id_, 'value': []}
                 for dim in range(0, config['dim']):
                     point['value'].append(float(row[dim]))
                 data.append(point)
     return data
-
 
 def read_config():
     config = {}
     try:
         with open(CONFIG, 'rb') as file_obj:
             for line in file_obj:
-                if line[0] != '#' and line.strip() !='':
+                if line[0] != '#' and line.strip() != '':
                     key, value = line.split('=')
                     if '.' in value.strip():
                         config[key.strip()] = float(value.strip())
                     else:
                         config[key.strip()] = int(value.strip())
     except:
-        print ("Error reading the configuration file.\
-            expected lines: param = value \n param = {eps, min_pts, dim}, \
-            value = {float, int, int}")
+        print("Error reading the configuration file. expected lines: param = value \n param = {eps, min_pts, dim}, value = {float, int, int}")
         sys.exit()
     return config
 
 def main():
-
     config = read_config()
     dbc = DBScanner(config)
     data = get_data(config)
-
     dbc.dbscan(data)
     dbc.export()
 
